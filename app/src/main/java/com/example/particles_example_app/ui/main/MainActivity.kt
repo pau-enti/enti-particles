e1cb07c103702a0fe3a518af88a5499d6bdeaf1a
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2021-11-07 20:25:18 +0100

    Comment main

--- app/src/main/java/com/example/particles_example_app/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/particles_example_app/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.particles_example_app
+package com.example.particles_example_app.ui.main
 
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
-import com.example.particles_example_app.ui.main.SectionsPagerAdapter
+import com.example.particles_example_app.R
+import com.example.particles_example_app.toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
@@ -13,20 +14,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Afegim el layout del main
         setContentView(R.layout.activity_main)
+
+        // Creem l'adapter pel ViewPager i el coloquem on toca
         val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
+
+        // Vinculem el ViewPager amb el TabLayout (gestor de pestanyes)
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
 
+        // Afegim una acció al botó flotant
+        val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", View.OnClickListener {
-                    // TODO
-                    // Do something
-                }).show()
+            Snackbar.make(view, "Do you want to know something?", Snackbar.LENGTH_LONG)
+                .setAction("YES") {
+                    toast("Maybe we live in a simulation")
+                }.show()
         }
     }
 }
