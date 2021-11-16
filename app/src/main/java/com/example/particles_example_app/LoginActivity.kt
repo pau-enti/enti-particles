2a7a8b79fa2cde941485aa3b03315ea6793b57cd
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2021-11-16 20:00:53 +0100

    Implemented data check on login

--- app/src/main/java/com/example/particles_example_app/LoginActivity.kt
+++ app/src/main/java/com/example/particles_example_app/LoginActivity.kt
@@ -3,17 +3,20 @@ package com.example.particles_example_app
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Patterns
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.particles_example_app.utils.makeSemitransparent
-import kotlinx.coroutines.NonCancellable.start
+import com.example.particles_example_app.ui.main.MainActivity
+import com.example.particles_example_app.utils.applyTransparency
+import com.example.particles_example_app.utils.toast
+import com.google.android.material.textfield.TextInputLayout
 
 
 class LoginActivity : AppCompatActivity() {
 
-    val ANIMATION_DURATION = 20000L // ms
+    val ANIMATION_DURATION = 30000L // ms
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +25,70 @@ class LoginActivity : AppCompatActivity() {
         // Amaga la appBar
         supportActionBar?.hide()
 
+        // Comencem animacions
         startAnimations()
+        val usernameInput = findViewById<TextInputLayout>(R.id.usernameInput).editText
+        val passwordInput = findViewById<TextInputLayout>(R.id.passwordInput).editText
+
+        usernameInput?.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                usernameInput.error = if (usernameInput.text.isValidEmail())
+                    null
+                 else
+                    "Invalid username"
+            }
+        }
+
+        passwordInput?.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                passwordInput.error = if (passwordInput.text.isValidPassword())
+                    null
+                else
+                    "Invalid password"
+            }
+        }
+
+        // Incorporem funcionalitats al botó de login
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            // Comprovem que les dades al formulari siguin correctes
+            if (validateData()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+                // Matem lla LoginAcitivity, perquè no volem que quedi "darrere" de la MainActivity
+                finish()
+            } else {
+                toast("Check the data")
+            }
+        }
     }
 
-    fun startAnimations() {
+    private fun validateData(): Boolean {
+        return findViewById<TextInputLayout>(R.id.usernameInput).editText?.text.isValidEmail()
+                && findViewById<TextInputLayout>(R.id.passwordInput).editText?.text.isValidPassword()
+    }
+
+    private fun CharSequence?.isValidEmail(): Boolean {
+        return if (this.isNullOrEmpty()) false
+        else Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+
+    private fun CharSequence?.isValidPassword(): Boolean {
+        return !this.isNullOrEmpty() && this.length >= 5
+    }
+
+    private fun TextInputLayout.isValidPassword(): Boolean {
+        val text = this.editText?.text
+        return if (text.isNullOrEmpty() || text.length < 5) {
+            this.error = "Invalid password"
+            false
+        } else {
+            this.error = null
+            true
+        }
+    }
+
+    private fun startAnimations() {
         // Anima icono
         ObjectAnimator.ofInt(
             findViewById(R.id.particleIcon), "colorFilter",
@@ -44,10 +107,10 @@ class LoginActivity : AppCompatActivity() {
         // Anima fondo
         ObjectAnimator.ofInt(
             findViewById(R.id.loginBackground), "backgroundColor",
-            applicationContext.getColor(R.color.gauge_bosons).makeSemitransparent(),
-            applicationContext.getColor(R.color.higgs).makeSemitransparent(),
-            applicationContext.getColor(R.color.leptons).makeSemitransparent(),
-            applicationContext.getColor(R.color.quarks).makeSemitransparent()
+            applicationContext.getColor(R.color.gauge_bosons).applyTransparency(),
+            applicationContext.getColor(R.color.higgs).applyTransparency(),
+            applicationContext.getColor(R.color.leptons).applyTransparency(),
+            applicationContext.getColor(R.color.quarks).applyTransparency()
         ).apply {
             duration = ANIMATION_DURATION
             setEvaluator(ArgbEvaluator())
--- app/src/main/java/com/example/particles_example_app/utils/Utils.kt
+++ app/src/main/java/com/example/particles_example_app/utils/Utils.kt
@@ -7,7 +7,7 @@ fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-fun Int.makeSemitransparent(): Int {
+fun Int.applyTransparency(): Int {
     // Primer fa una màscara amb l'AND per suprimir la transparència que té el color
     // El valor de alpha es troba als 2 primers bytes
     // Seguidament, amb l'OR li sumem el valor de la transparència que volem (50%)
