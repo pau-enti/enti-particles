5424cc3213bb5c4af6ed8859b592be563bfdfa78
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2021-11-21 19:36:01 +0100

    Toast opened to print any object

--- app/src/main/java/com/example/particles_example_app/utils/Utils.kt
+++ app/src/main/java/com/example/particles_example_app/utils/Utils.kt
@@ -3,8 +3,10 @@ package com.example.particles_example_app.utils
 import android.content.Context
 import android.widget.Toast
 
-fun Context.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+fun Context.toast(message: Any?) {
+    message?.let {
+        Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+    }
 }
 
 fun Int.applyTransparency(): Int {
