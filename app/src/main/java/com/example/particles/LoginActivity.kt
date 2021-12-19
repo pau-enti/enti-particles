e734baa30fc5d448e9246daa740efc91ae73e705
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2021-12-19 17:03:14 +0100

    Sign up implemented

--- app/src/main/java/com/example/particles/LoginActivity.kt
+++ app/src/main/java/com/example/particles/LoginActivity.kt
@@ -3,23 +3,48 @@ package com.example.particles
 import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.particles.databinding.ActivityLoginBinding
 import com.example.particles.ui.main.MainActivity
 import com.example.particles.utils.applyTransparency
 import com.example.particles.utils.toast
-import com.google.firebase.auth.FederatedAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    val ANIMATION_DURATION = 30000L // ms
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
-    lateinit var binding: ActivityLoginBinding
-    lateinit var firebaseAuth: FirebaseAuth
+    companion object {
+        private const val ANIMATION_DURATION = 30000L // ms
+
+        fun checkLogin(user: String?, pass: String?): Boolean {
+            return user.isValidEmail() && pass.isValidPassword()
+        }
+
+        fun String?.isValidEmail(): Boolean {
+            return if (this.isNullOrEmpty()) false
+            else Patterns.EMAIL_ADDRESS.matcher(this).matches()
+        }
+
+        fun String?.isValidPassword(): Boolean {
+            return !this.isNullOrEmpty() && this.length >= 5
+        }
+    }
+
+    private val registerLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                finish()
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,41 +57,19 @@ class LoginActivity : AppCompatActivity() {
         startAnimations()
         firebaseAuth = FirebaseAuth.getInstance()
 
-        // Quan l'usuari acaba d'escriure i passa al següent camp (es perd el focus), llavors comprovem
-        // que les dades que ha introduit són correctes
-        binding.usernameInput.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) {
-                binding.usernameInput.error =
-                        // Si les dades són correctes, esborrem l'error (per si n'hi hagués)
-                    if (binding.usernameInput.text.toString().isValidEmail())
-                        null
-
-                    // Si les dades no són correctes, mostrem aquest error
-                    else
-                        "Invalid username"
-            }
-        }
-
-        // Fem el mateix check, però pel password
-        binding.passwordInput.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) {
-                binding.passwordInput.error =
-                    if (binding.passwordInput.text.toString().isValidPassword())
-                        null
-                    else
-                        "Invalid password"
-            }
-        }
+        setDataCheckers()
 
         // Incorporem funcionalitats al botó de login
         binding.loginButton.setOnClickListener {
 
-            val user = binding.usernameInput.text.toString()
+            val user = binding.emailInput.text.toString()
             val pass = binding.passwordInput.text.toString()
+
+            binding.loginProgress.visibility = View.VISIBLE
 
             // Comprovem que les dades al formulari siguin correctes
             if (!checkLogin(user, pass)) {
-                toast("Check the data")
+                toast(getString(R.string.err_login_data))
                 return@setOnClickListener
             }
 
@@ -81,12 +84,14 @@ class LoginActivity : AppCompatActivity() {
                 // Matem la LoginAcitivity, perquè no volem que quedi "darrere" de la MainActivity
                 finish()
             }.addOnFailureListener {
-                toast("Incorrect user or password")
+                toast(getString(R.string.err_login))
+            }.addOnCompleteListener {
+                binding.loginProgress.visibility = View.INVISIBLE
             }
         }
 
         binding.register.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
+            registerLauncher.launch(Intent(this, RegisterActivity::class.java))
         }
 
         // Skip login
@@ -96,19 +101,32 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    private fun setDataCheckers() {
+        // Quan l'usuari acaba d'escriure i passa al següent camp (es perd el focus), llavors comprovem
+        // que les dades que ha introduit són correctes
+        binding.emailInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                binding.emailInput.error =
+                        // Si les dades són correctes, esborrem l'error (per si n'hi hagués)
+                    if (binding.emailInput.text.toString().isValidEmail())
+                        null
 
-    private fun checkLogin(user: String?, pass: String?): Boolean {
-        return user.isValidEmail() && pass.isValidPassword()
-    }
+                    // Si les dades no són correctes, mostrem aquest error
+                    else
+                        getString(R.string.err_invalid_username)
+            }
+        }
 
-
-    private fun String?.isValidEmail(): Boolean {
-        return if (this.isNullOrEmpty()) false
-        else Patterns.EMAIL_ADDRESS.matcher(this).matches()
-    }
-
-    private fun String?.isValidPassword(): Boolean {
-        return !this.isNullOrEmpty() && this.length >= 5
+        // Fem el mateix check, però pel password
+        binding.passwordInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                binding.passwordInput.error =
+                    if (binding.passwordInput.text.toString().isValidPassword())
+                        null
+                    else
+                        getString(R.string.err_invalid_password)
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/particles/RegisterActivity.kt
+++ app/src/main/java/com/example/particles/RegisterActivity.kt
@@ -1,12 +1,28 @@
 package com.example.particles
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.particles.LoginActivity.Companion.isValidEmail
+import com.example.particles.LoginActivity.Companion.isValidPassword
 import com.example.particles.databinding.ActivityRegisterBinding
+import com.example.particles.ui.main.MainActivity
+import com.example.particles.utils.toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
 
 class RegisterActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityRegisterBinding
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+//    private lateinit var firebase: Firebase
+
+    private fun String?.isValidName(): Boolean {
+        return !this.isNullOrEmpty()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +30,76 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        setDataCheckers()
 
+        binding.signUpButton.setOnClickListener {
+            val user = binding.emailInput.text.toString()
+            val name = binding.nameInput.text.toString()
+            val surname = binding.lastNameInput.text.toString()
+            val pass = binding.passwordInput.text.toString()
+            val rpass = binding.repeatPasswordInput.text.toString()
+
+
+            if (checkSignUp(user, name, surname, pass, rpass)) {
+                binding.singnUpProgress.visibility = View.VISIBLE
+
+                firebaseAuth.createUserWithEmailAndPassword(user, pass).addOnSuccessListener {
+                    firebaseAuth.signInWithEmailAndPassword(user, pass)
+//                    firebase.app.
+                    toast(getString(R.string.register_ok))
+
+                    // Notify the user has registered successfully
+                    setResult(Activity.RESULT_OK)
+                    finish()
+                    startActivity(Intent(this, MainActivity::class.java))
+                }.addOnFailureListener {
+                    toast(getString(R.string.err_general))
+                }.addOnCompleteListener {
+                    binding.singnUpProgress.visibility = View.INVISIBLE
+                }
+
+            } else
+                toast(getString(R.string.err_login_data))
+
+        }
+
+    }
+
+    private fun checkSignUp(
+        email: String,
+        name: String,
+        surname: String,
+        pass: String,
+        rpass: String
+    ): Boolean {
+        return name.isValidName() && surname.isValidName() && email.isValidEmail() && pass.isValidPassword() && rpass == pass
+    }
+
+    private fun setDataCheckers() {
+        binding.emailInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && !binding.emailInput.text.toString().isValidEmail())
+                binding.emailInput.error = getString(R.string.err_login_data)
+        }
+
+        binding.nameInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && !binding.nameInput.text.toString().isValidName())
+                binding.nameInput.error = getString(R.string.err_login_data)
+        }
+
+        binding.lastNameInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && !binding.lastNameInput.text.toString().isValidName())
+                binding.lastNameInput.error = getString(R.string.err_login_data)
+        }
+
+        binding.passwordInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && !binding.passwordInput.text.toString().isValidPassword())
+                binding.passwordInput.error = getString(R.string.err_login_data)
+        }
+
+        binding.repeatPasswordInput.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus && binding.repeatPasswordInput.text == binding.passwordInput.text)
+                binding.repeatPasswordInput.error = getString(R.string.err_password_mismatches)
+        }
     }
 }
