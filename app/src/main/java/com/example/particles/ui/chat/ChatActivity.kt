67f20dc546f4f7c9a2e9fa1f4c5ca2852975456b
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2022-04-26 13:21:16 +0200

    Realtime database

--- app/src/main/java/com/example/particles/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/example/particles/ui/chat/ChatActivity.kt
@@ -1,14 +1,20 @@
 package com.example.particles.ui.chat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.particles.databinding.ActivityChatBinding
+import com.example.particles.utils.toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class ChatActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityChatBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +22,35 @@ class ChatActivity : AppCompatActivity() {
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+        database = Firebase.database.reference
 
-        myRef.setValue("Hello, World!")
+        binding.messageSend.setOnClickListener {
+            database.child("users").child("user1").setValue("Gerard")
+            database.child("glossary").setValue("Gerard4")
+
+            database.child("glossary").get().addOnCompleteListener {
+                toast(it.result)
+            }
+
+            database.push()
+
+        }
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+//                val post = dataSnapshot.getValue<Post>()
+                // ...
+                toast(dataSnapshot.value)
+                println("ARAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                toast(databaseError.message)
+            }
+        }
+
+        database.child("glossary").addValueEventListener(postListener)
     }
 }
