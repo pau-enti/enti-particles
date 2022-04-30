f1cd7bdc182d7e8a949d532e1b0b72c3b1e9bc77
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2022-04-30 15:27:40 +0200

    Created chatsListActivity

---
+++ app/src/main/java/com/example/particles/ui/chats/ChatsListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.particles.ui.chats
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chats_list)
+    }
+}
