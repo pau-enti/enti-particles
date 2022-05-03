3bf30f5a953def67c74fea7630ee56aaae652c0a
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2022-05-03 13:50:02 +0200

    Contacts list implemented

--- app/src/main/java/com/example/particles/ui/contacts/Contact.kt
+++ app/src/main/java/com/example/particles/ui/contacts/Contact.kt
@@ -1,9 +1,10 @@
 package com.example.particles.ui.contacts
 
+import java.io.Serializable
 import java.net.URI
 
 data class Contact(
     val name: String,
     val userId: String,
     val imageFile: URI?=null
-)
+): Serializable
--- app/src/main/java/com/example/particles/ui/contacts/ContactsViewModel.kt
+++ app/src/main/java/com/example/particles/ui/contacts/ContactsViewModel.kt
@@ -3,13 +3,16 @@ package com.example.particles.ui.contacts
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.io.FileNotFoundException
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
 class ContactsViewModel : ViewModel() {
     val contacts = MutableLiveData<ArrayList<Contact>>()
     private val dataFilename = "chat_contacts.dat"
+
+    private fun MutableLiveData<*>.notifyObservers() {
+        value = value // xd
+    }
 
     private fun saveContacts(context: Context) {
         val data = contacts.value ?: return
@@ -20,23 +23,30 @@ class ContactsViewModel : ViewModel() {
         }
     }
 
-    @Suppress("UNCHECKED_CAST")
     fun loadContacts(context: Context) {
         try {
             context.openFileInput(dataFilename).use {
                 val iin = ObjectInputStream(it)
-                val data = iin.readObject() ?: return
+                val data = iin.readObject() ?: return@use
+
+                @Suppress("UNCHECKED_CAST")
                 val readContacts = data as ArrayList<Contact>
-                contacts.postValue(readContacts)
+
+                contacts.value = readContacts
                 iin.close()
             }
-        } catch (e: FileNotFoundException) {
+        } catch (e: Exception) {
             // pass
         }
+
+        if (contacts.value == null)
+            contacts.value = arrayListOf()
     }
+
 
     fun addContact(context: Context, contact: Contact) {
         contacts.value?.add(contact)
+        contacts.notifyObservers()
         saveContacts(context)
     }
 }
